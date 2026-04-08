3ef30ae5f8c62c3581ad995cd20418cd49fe436a
Jenna Moss <jenna.moss@hotmail.com>
2026-04-08 18:57:03 -0400

    fixing error

--- create.php
+++ create.php
@@ -93,7 +93,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <h2>Create Resume</h2>
 
-<?php foreach ($errors as $err): ?>
+<?php foreach ($error as $err): ?>
     <div class="alert alert-danger"><?= $err ?></div>
 <?php endforeach; ?>
 
