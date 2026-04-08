5c687534b6787be3518576d91acc1096a488c774
Jenna Moss <jenna.moss@hotmail.com>
2026-04-08 18:00:51 -0400

    adding logic to check if user is logged in

--- create.php
+++ create.php
@@ -1,13 +1,12 @@
-<?php include "config.php"; ?>
-
-<!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
-
-<div class="container mt-5">
-
-
 <?php
+session_start();
+include "config.php";
+
+// Check if user is logged in
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
 
 // function to sanitize user input
 function clean_input($data) {
